1c6610d4bbf26ff99095caa169fe5d600095c80f
Emily Schur <ejschur@gmail.com>
2024-11-25 17:45:35 -0800

    Work on marker filtering on load

--- src/components/MapWithMarkers/MapWithMarkers.jsx
+++ src/components/MapWithMarkers/MapWithMarkers.jsx
@@ -1,9 +1,8 @@
 import PetMarkers from "../PetMarkers/PetMarkers.jsx";
 import CentralMarker from "../CentralMarker/CentralMarker.jsx";
 import * as googleMapsApi from "../../api/googleMaps.js";
-import { Map, useMap } from "@vis.gl/react-google-maps";
+import { Map } from "@vis.gl/react-google-maps";
 import PropTypes from "prop-types";
-import { useCallback, useEffect } from "react";
 
 const mapContainerStyle = { width: "100%", height: "100%" };
 
@@ -13,52 +12,27 @@ function MapWithMarkers({
   updateVisibleMarkers = null,
   centralMarker = {},
 }) {
-  const map = useMap();
-
-  const filterForVisibleMarkers = useCallback(
-    (bounds) => {
-      const northEast = { lat: bounds.north, lng: bounds.east };
-      const southWest = { lat: bounds.south, lng: bounds.west };
-
-      const filteredMarkers = markersList.filter(
-        (marker) =>
-          marker.lat >= southWest.lat &&
-          marker.lat <= northEast.lat &&
-          marker.lng >= southWest.lng &&
-          marker.lng <= northEast.lng
-      );
-      if (updateVisibleMarkers) {
-        updateVisibleMarkers(filteredMarkers);
-      }
-    },
-    [markersList, updateVisibleMarkers]
-  );
-
-  useEffect(() => {
-    if (!map) return;
-
-    map.panTo(mapLocation);
-  }, [mapLocation, map]);
-
-  useEffect(() => {
-    if (!map) return;
-
-    const initialBounds = map.getBounds();
-    if (initialBounds) {
-      filterForVisibleMarkers(initialBounds);
-    }
-  }, [map, filterForVisibleMarkers]);
-
   const handleBoundsChanged = (ev) => {
     const bounds = ev.detail.bounds;
-    filterForVisibleMarkers(bounds);
+    const northEast = { lat: bounds.north, lng: bounds.east };
+    const southWest = { lat: bounds.south, lng: bounds.west };
+
+    const filteredMarkers = markersList.filter(
+      (marker) =>
+        marker.lat >= southWest.lat &&
+        marker.lat <= northEast.lat &&
+        marker.lng >= southWest.lng &&
+        marker.lng <= northEast.lng
+    );
+    if (updateVisibleMarkers) {
+      updateVisibleMarkers(filteredMarkers);
+    }
   };
 
   return (
     <Map
       style={mapContainerStyle}
       defaultZoom={14}
-      reuseMaps={true}
       mapId={googleMapsApi.mapId}
       defaultCenter={mapLocation}
       onCameraChanged={(ev) => handleBoundsChanged(ev)}
--- src/pages/PetDetailsPage/PetDetailsPage.jsx
+++ src/pages/PetDetailsPage/PetDetailsPage.jsx
@@ -1,5 +1,5 @@
 import "./PetDetailsPage.scss";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import * as backend from "../../api/backend.js";
 import { useCallback, useEffect, useState } from "react";
 import { Chip, List, Skeleton, Typography } from "@mui/material";
@@ -7,9 +7,11 @@ import { format } from "date-fns";
 import MapWithMarkers from "../../components/MapWithMarkers/MapWithMarkers.jsx";
 import SightingForm from "../../components/SightingForm/SIghtingForm.jsx";
 import SightingCard from "../../components/SightingCard/SightingCard.jsx";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 function PetDetailsPage() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [petData, setPetData] = useState({});
   const [petSightings, setPetSightings] = useState([]);
   const [mapLocation, setMapLocation] = useState({});
@@ -35,110 +37,130 @@ function PetDetailsPage() {
 
   return (
     <div className="pet-details-page">
-      <div className="pet-details-page__details-and-form">
-        <div className="pet-details-page__details">
-          <div className="pet-details-page__image-container">
-            <img
-              src={petData.pet_image}
-              alt={`${petData.pet_name}'s picture`}
-              className="pet-details-page__image"
-            />
-          </div>
-          <div className="pet-details-page__header-container">
+      <Typography
+        variant="h4"
+        component="h1"
+        className="pet-details-page__title"
+        sx={{ fontWeight: "medium", m: { xs: "1rem 0", sm: "1.5rem 0" } }}
+      >
+        <ArrowBackIcon
+          className="pet-details-page__title-icon"
+          color="secondary"
+          sx={{ mr: 1 }}
+          onClick={() => navigate(-1)}
+        />
+        Pet Details
+      </Typography>
+      <div className="pet-details-page__layout">
+        <div className="pet-details-page__details-and-form">
+          <div className="pet-details-page__details">
+            <div className="pet-details-page__image-container">
+              <img
+                src={petData.pet_image}
+                alt={`${petData.pet_name}'s picture`}
+                className="pet-details-page__image"
+              />
+            </div>
+            <div className="pet-details-page__header-container">
+              <Typography
+                variant="h4"
+                component="h2"
+                className="pet-details-page__pet-name"
+                sx={{ fontWeight: "medium" }}
+              >
+                {petData.pet_name}
+              </Typography>
+              <div className="pet-details-page__status-container">
+                <Chip
+                  className={"pet-details-page__status"}
+                  variant="outlined"
+                  size="small"
+                  label={petData.status === "lost" ? "Missing" : "Found"}
+                  color={petData.status === "lost" ? "error" : "success"}
+                />
+                <Typography
+                  variant="caption"
+                  component="p"
+                  className="pet-details-page__missing-since"
+                >
+                  Since{" "}
+                  {petData.missing_since &&
+                    format(petData.missing_since, "MMM do")}
+                </Typography>
+              </div>
+            </div>
             <Typography
-              variant="h4"
-              component="h1"
-              className="pet-details-page__title"
+              variant="body2"
+              component="p"
+              className="pet-details-page__pet-attributes"
+              sx={{ mb: 1 }}
+            >
+              Age: {petData.pet_age} | Size: {petData.pet_size} |{" "}
+              {petData.pet_temperament}
+            </Typography>
+            <Typography
+              variant="body2"
+              component="p"
+              color="secondary"
+              className="pet-details-page__pet-city"
+              sx={{ mb: 1, fontWeight: "medium" }}
+            >
+              {petData.city}
+            </Typography>
+            <Typography
+              variant="body1"
+              component="p"
+              className="pet-details-page__description"
+              sx={{ mb: "1rem" }}
+            >
+              {petData.description}
+            </Typography>
+          </div>
+          <div className="pet-details-page__sighting-form">
+            <Typography
+              variant="h7"
+              component="h3"
               sx={{ fontWeight: "medium" }}
             >
-              {petData.pet_name}
+              Have you seen {petData.pet_name}?
             </Typography>
-            <div className="pet-details-page__status-container">
-              <Chip
-                className={"pet-details-page__status"}
-                variant="outlined"
-                size="small"
-                label={petData.status === "lost" ? "Missing" : "Found"}
-                color={petData.status === "lost" ? "error" : "success"}
-              />
-              <Typography
-                variant="caption"
-                component="p"
-                className="pet-details-page__missing-since"
-              >
-                Since{" "}
-                {petData.missing_since &&
-                  format(petData.missing_since, "MMM do")}
-              </Typography>
-            </div>
+            <SightingForm petId={id} handleNewSighting={handleNewSighting} />
           </div>
-          <Typography
-            variant="body2"
-            component="p"
-            className="pet-details-page__pet-attributes"
-            sx={{ mb: 1 }}
-          >
-            Age: {petData.pet_age} | Size: {petData.pet_size} |{" "}
-            {petData.pet_temperament}
-          </Typography>
-          <Typography
-            variant="body2"
-            component="p"
-            color="secondary"
-            className="pet-details-page__pet-city"
-            sx={{ mb: 1, fontWeight: "medium" }}
-          >
-            {petData.city}
-          </Typography>
-          <Typography
-            variant="body1"
-            component="p"
-            className="pet-details-page__description"
-            sx={{ mb: "1rem" }}
-          >
-            {petData.description}
-          </Typography>
         </div>
-        <div className="pet-details-page__sighting-form">
-          <Typography variant="h7" component="h3" sx={{ fontWeight: "medium" }}>
-            Have you seen {petData.pet_name}?
+        <div className="pet-details-page__map-and-results">
+          <Typography
+            variant="h6"
+            component="h3"
+            sx={{ m: { xs: "1rem 0", sm: "0.5rem 0 0.25rem" } }}
+            className="pet-details-page__sightings-title"
+          >
+            Sightings of {petData.pet_name}
           </Typography>
-          <SightingForm petId={id} handleNewSighting={handleNewSighting} />
-        </div>
-      </div>
-      <div className="pet-details-page__map-and-results">
-        <Typography
-          variant="h6"
-          component="h3"
-          sx={{ m: { xs: "1rem 0", sm: "0.5rem 0 0.25rem" } }}
-          className="pet-details-page__sightings-title"
-        >
-          Sightings of {petData.pet_name}
-        </Typography>
-        <div className="pet-details-page__map-container">
-          {mapLocation.lat ? (
-            <MapWithMarkers
-              markersList={petSightings}
-              mapLocation={mapLocation}
-              centralMarker={{ lat: petData.lat, lng: petData.lng }}
-            />
-          ) : (
-            <Skeleton variant="rectangle" sx={{ height: "300px" }} />
-          )}
-        </div>
-        <List
-          className="pet-details-page__sightings-list"
-          sx={{ padding: "0.5rem" }}
-        >
-          {petSightings.length > 0 &&
-            petSightings.map((sighting) => (
-              <SightingCard
-                sightingData={sighting}
-                handleClick={handleSightingCardClick}
-                key={sighting.id}
+          <div className="pet-details-page__map-container">
+            {mapLocation.lat ? (
+              <MapWithMarkers
+                markersList={petSightings}
+                mapLocation={mapLocation}
+                centralMarker={{ lat: petData.lat, lng: petData.lng }}
               />
-            ))}
-        </List>
+            ) : (
+              <Skeleton variant="rectangle" sx={{ height: "300px" }} />
+            )}
+          </div>
+          <List
+            className="pet-details-page__sightings-list"
+            sx={{ padding: "0.5rem" }}
+          >
+            {petSightings.length > 0 &&
+              petSightings.map((sighting) => (
+                <SightingCard
+                  sightingData={sighting}
+                  handleClick={handleSightingCardClick}
+                  key={sighting.id}
+                />
+              ))}
+          </List>
+        </div>
       </div>
     </div>
   );
