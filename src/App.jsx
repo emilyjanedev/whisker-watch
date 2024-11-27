43ade30c5e68e570e0dd349d5b9fee3a96ad44f4
Emily Schur <ejschur@gmail.com>
2024-11-27 10:04:20 -0800

    Implement v1 of login/signup page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PetDetailsPage from "./pages/PetDetailsPage/PetDetailsPage";
 import AddPetPage from "./pages/AddPetPage/AddPetPage";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import "./styles/globals.scss";
+import LoginPage from "./pages/LoginPage/LoginPage.jsx";
 
 function App() {
   return (
@@ -13,6 +14,8 @@ function App() {
         <Route path="/" element={<PetMapPage />} />
         <Route path="/pets/:id" element={<PetDetailsPage />} />
         <Route path="/pets/add" element={<AddPetPage />} />
+        <Route path="/login" element={<LoginPage action="login" />} />
+        <Route path="/sign-up" element={<LoginPage action="signup" />} />
       </Routes>
     </BrowserRouter>
   );
