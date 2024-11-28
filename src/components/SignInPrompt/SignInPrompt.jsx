26f7aae89b8b701e6480df943dc7ae1b2a9e9147
Emily Schur <ejschur@gmail.com>
2024-11-28 10:56:13 -0800

    Style SignInPromt

--- src/components/SignInPrompt/SignInPrompt.jsx
+++ src/components/SignInPrompt/SignInPrompt.jsx
@@ -1,24 +1,40 @@
 import { Link } from "react-router-dom";
 import StyledButton from "../../components/StyledButton/StyledButton.jsx";
 import PropTypes from "prop-types";
+import { Container, Typography } from "@mui/material";
 
 function SignInPrompt({ action }) {
   return (
-    <>
-      <p>
+    <Container
+      maxWidth="xs"
+      sx={{
+        textAlign: "center",
+        p: 4,
+        mt: 1.5,
+        borderRadius: "20px",
+        bgcolor: "#f7f7f7",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "1rem",
+      }}
+    >
+      <Typography variant="body1">
         {action === "sighting"
-          ? "Please sign in to post a sighting."
+          ? "Please sign in to report a sighting."
           : "Please sign in to report a lost pet."}
-      </p>
+      </Typography>
+
       <StyledButton
         variant="contained"
         disableElevation
         component={Link}
         to="/login"
+        sx={{ width: "50%" }}
       >
         Sign In
       </StyledButton>
-    </>
+    </Container>
   );
 }
 
